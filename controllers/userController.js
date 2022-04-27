cfdf497a540e93f5ad23828d60743cacd53169ac
mrakesh0608 <mrakesh0608@gmail.com>
2022-04-27 13:17:37 +0530

    d

--- controllers/userController.js
+++ controllers/userController.js
@@ -167,7 +167,7 @@ module.exports.updateYourCoursesTask = async (req,res)=>{
         
         console.log("gg\n"+course);
         const courseMD = await Course.findOne( {_id:courseID,email});
-        console.log(courseMD[0].tasks);
+        console.log(Object.keys(courseMD));
 
         res.status(200).json( {updated:true} );
         console.log("Task Updated");
